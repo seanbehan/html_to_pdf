328e5d7773352f4b6e0aee8be46bf1634d99fbfa
Sean Behan <inbox@seanbehan.com>
2016-01-10 21:17:18 -0500

    rewrite to use system command

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
 from flask import Flask, request, send_file, jsonify
-from pdfkit import from_string as pdf_from_string, from_url as pdf_from_url, configuration as pdfkit_config
 from uuid import uuid4
 from os import environ as env, getcwd
-from os.path import basename, exists
+from os.path import basename, exists, system
 
+WKHTMLTOPDF = env.get('WKHTMLTOPDF', '{}/bin/wkhtmltopdf'.format(getcwd()))
 
 app = Flask(__name__)
-pdf_config = pdfkit_config(wkhtmltopdf=env.get('WKHTMLTOPDF_EXEC', '{}/bin/wkhtmltopdf'.format(getcwd())))
+
+def make_pdf(source=''):
+    pdf_file = '{}/pdfs/{}.pdf'.format(getcwd(), str(uuid4()))
+
+    if source.startswith('http'):
+        system('%s %s %s' % (WKHTMLTOPDF, source, pdf_file))
+        return pdf_file
+
+    source_file = '{}/pdfs/{}.html'.format(getcwd(), str(uuid4()))
+    with open(source_file) as f:
+        f.write(source)
+    system('%s %s %s' % (WKHTMLTOPDF, source_file, pdf_file))
+    return pdf_file
 
 @app.route("/")
 def home():
-    return """<html><h3>PDF From HTML</h3><form action='/pdf'><input size=100 type='text' name='source' placeholder='Enter a valid URL to get a PDF'/><button onclick='this.innerHTML=Submitted'>Go</button></form></body></html>"""
+    return """<html>
+    <h3>PDF From HTML</h3>
+    <form action='/pdf'>
+    <input size=100 type='text' name='source' placeholder='Enter a valid URL to get a PDF'/>
+    <button>Go</button>
+    </form></body></html>"""
 
 @app.route("/pdf", methods=["GET"])
 def make_pdf_from_url():
-    pdf_file = '{}/pdfs/{}.pdf'.format(getcwd(), str(uuid4()))
     source = str(request.args.get('url'))
-
-    try:
-        pdf_from_url(source, pdf_file, configuration=pdf_config)
-    except:
-        return "We were unable to make a PDF from this url."
-
+    pdf_file = make_pdf(source)
     return send_file(pdf_file, as_attachment=False)
 
 @app.route("/pdf", methods=["POST"])
 def make_pdf_from_html():
-    pdf_file = '{}/pdfs/{}.pdf'.format(getcwd(), str(uuid4()))
+    source = unicode(request.data, 'utf-8')
+    pdf_file = make_pdf(source)
 
-    try:
-        pdf_from_string(unicode(request.data, 'utf-8'), pdf_file, configuration=pdf_config)
-    except:
-        pass
-
-    if exists(pdf_file):
-        return send_file(pdf_file, as_attachment=True)
-
-    return "We were unable to make a PDF for you."
+    return send_file(pdf_file, as_attachment=True)
 
 if __name__=='__main__':
     app.run(debug=True)
